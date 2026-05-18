eb980e64e4ddbfd8f36642049003f49683faa2e5
Tashwill <tashwilladvice02@gmail.com>
2026-05-18 11:18:19 +0200

    #1. Asking which folder to scan #   For a quick test, i will use the "Downloads" folder

--- PythonTutFileManagement&CleanupAutomationTool/PythonTutFileManagement_CleanupAutomationTool.py
+++ PythonTutFileManagement&CleanupAutomationTool/PythonTutFileManagement_CleanupAutomationTool.py
@@ -1,4 +1,9 @@
 import os # This is python libary , allowing access to windows machine 
 
+print("Tashwill's Automation Tool: Folder Scan")
+print("-" *35)
 
+#1. Asking which folder to scan 
+#   For a quick test, i will use the "Downloads" folder
+folder_path = input("C:\Users\USER\Downloads")
 
